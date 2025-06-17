656e555d2c46b15ad918e8d103171dddb6f7afb5
Jose Fajardo <josefaja207@gmail.com>
2025-06-17 00:30:07 -0500

    ..

--- Tienda final/src/reportes/ReporteVentas.java
+++ Tienda final/src/reportes/ReporteVentas.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -23,6 +24,32 @@ public class ReporteVentas extends Reportes {
             }
         }
     }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reportes;
+
+import Entities.Venta;
+import java.util.List;
+
+public class ReporteVentas extends Reportes {
+
+        public ReporteVentas(String titulo, List<Venta> ventas) {
+            super(titulo, ventas);
+        }
+
+        @Override
+        public void generarReporte() {
+            imprimirEncabezado();
+            System.out.println("Detalle de Ventas:");
+            for (Venta venta : ventas) {
+                System.out.println(venta.toString());
+            }
+        }
+    }
+>>>>>>> parent of b423e5c (Merge pull request #33 from JuanHolguin-bot/Feature/ReporteBajoStock)
 =======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
--- Tienda final/src/reportes/Reportes.java
+++ Tienda final/src/reportes/Reportes.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -35,6 +36,44 @@ public abstract class Reportes {
         System.out.println("Fecha de generación: " + fechaGeneracion);
         System.out.println("============================");
     }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reportes;
+
+import Entities.Venta;
+import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author User
+ */
+public abstract class Reportes {
+    protected String titulo;
+    protected List<Venta> ventas;
+    protected String fechaGeneracion;
+
+    public Reportes(String titulo, List<Venta> ventas) {
+        this.titulo = titulo;
+        this.ventas = ventas;
+        this.fechaGeneracion = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")); // Fecha actual
+    }
+
+    // Método abstracto que las subclases deben implementar
+    public abstract void generarReporte();
+
+    // Método auxiliar opcional para imprimir encabezado
+    protected void imprimirEncabezado() {
+        System.out.println("============================");
+        System.out.println("Reporte: " + titulo);
+        System.out.println("Fecha de generación: " + fechaGeneracion);
+        System.out.println("============================");
+    }
+>>>>>>> parent of b423e5c (Merge pull request #33 from JuanHolguin-bot/Feature/ReporteBajoStock)
 =======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
